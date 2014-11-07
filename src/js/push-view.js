661088c512f4e7b7aadbf2afb0f166f42743916a
Baptiste Rios Campo <baptiste.rios@gmail.com>
2014-11-07 22:41:01 +0100

    better closing method, avoid jumps when closing animation

--- src/js/push-view.js
+++ src/js/push-view.js
@@ -1,78 +1,87 @@
-(function(exp, b, c, s) {
-    var opened,
-        canPushState = /^f/.test(history.pushState),
-        isSupported = b.classList && transitionEndEventName(),
-        cb,
-        hasPushState = false;
+(function (exp, b, c, s) {
+  var opened,
+    canPushState = /^f/.test(history.pushState),
+    isSupported = b.classList && transitionEndEventName(),
+    cb,
+    hasPushState = false;
 
-    exp.$pushView = {
-        init: function(config) {
-            var _t = this;
+  exp.$pushView = {
+    init: function (config) {
+      var _t = this;
 
-            if(!isSupported) {
-                return false;
-            }
+      if (!isSupported) {
+        return false;
+      }
 
-            if(config) {
-                if(config.elt) {
-                    b = config.elt;
-                }
-                if(config.prefix) {
-                    c = config.prefix;
-                }
-                if(config.pushState && canPushState) {
-                    hasPushState = config.pushState;
-                }
-            }
-
-            b.classList.add(c + '-container');
-
-            b.addEventListener(transitionEndEventName(), function(evt) {
-                // Confirm the target is pushView container (b)
-                if(evt.target.classList.contains(c + '-container--animating')) {
-                    b.classList.remove(c + '-container--animating');
-                    if(cb) {
-                        cb();
-                    }
-                }
-            });
-
-            if(hasPushState) {
-                window.onpopstate = function () {
-                    if(opened) _t.close();
-                };
-            }
-        },
-        open: function(callback) {
-            if(!isSupported) {
-                if(callback) callback();
-                return false;
-            }
-
-            cb = callback;
-            b.classList.add(c + '-container--animating');
-            b.classList.add(c + '-container--opened');
-
-            if(hasPushState) {
-                history.pushState(s, null, null);
-            }
-
-            opened = true;
-        },
-        close: function(callback) {
-            if(!isSupported) {
-                if(callback) callback();
-                return false;
-            }
-
-            cb = callback;
-            b.classList.add(c + '-container--animating');
-            b.classList.remove(c + '-container--opened');
-            if(hasPushState && history.state === s) {
-                history.back();
-            }
-
-            opened = false;
+      if (config) {
+        if (config.elt) {
+          b = config.elt;
         }
+        if (config.prefix) {
+          c = config.prefix;
+        }
+        if (config.pushState && canPushState) {
+          hasPushState = config.pushState;
+        }
+      }
+
+      b.classList.add(c + '-container');
+
+      b.addEventListener(transitionEndEventName(), function (evt) {
+        var cList = evt.target.classList;
+
+        // Confirm the target is pushView container (b)
+        if (cList.contains(c + '-container--animating')) {
+          cList.remove(c + '-container--animating');
+
+          if(cList.contains(c + '-container--closing')) {
+            cList.remove(c + '-container--closing');
+            cList.remove(c + '-container--opened');
+          }
+
+          if (cb) {
+            cb();
+          }
+        }
+      });
+
+      if (hasPushState) {
+        window.onpopstate = function () {
+          if (opened) _t.close();
+        };
+      }
+    },
+    open: function (callback) {
+      if (!isSupported) {
+        if (callback) callback();
+        return false;
+      }
+
+      cb = callback;
+      b.classList.add(c + '-container--animating');
+      b.classList.add(c + '-container--opened');
+
+      if (hasPushState) {
+        history.pushState(s, null, null);
+      }
+
+      opened = true;
+    },
+    close: function (callback) {
+      if (!isSupported) {
+        if (callback) callback();
+        return false;
+      }
+
+      cb = callback;
+      b.classList.add(c + '-container--animating');
+      b.classList.add(c + '-container--closing');
+      //b.classList.remove(c + '-container--opened');
+      if (hasPushState && history.state === s) {
+        history.back();
+      }
+
+      opened = false;
     }
+  }
 })(window, document.body, 'push', 'pushViewOpened');
--- src/js/transitionEnd.js
+++ src/js/transitionEnd.js
@@ -1,17 +1,17 @@
-function transitionEndEventName () {
-    var i,
-        undefined,
-        el = document.createElement('div'),
-        transitions = {
-            'transition':'transitionend',
-            'OTransition':'otransitionend',  // oTransitionEnd in very old Opera
-            'MozTransition':'transitionend',
-            'WebkitTransition':'webkitTransitionEnd'
-        };
+function transitionEndEventName() {
+  var i,
+    undefined,
+    el = document.createElement('div'),
+    transitions = {
+      'transition': 'transitionend',
+      'OTransition': 'otransitionend',  // oTransitionEnd in very old Opera
+      'MozTransition': 'transitionend',
+      'WebkitTransition': 'webkitTransitionEnd'
+    };
 
-    for (i in transitions) {
-        if (transitions.hasOwnProperty(i) && el.style[i] !== undefined) {
-            return transitions[i];
-        }
+  for (i in transitions) {
+    if (transitions.hasOwnProperty(i) && el.style[i] !== undefined) {
+      return transitions[i];
     }
+  }
 }
